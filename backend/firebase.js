0fde53157696d686ebd30d790f248fa751ed5337
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-29 20:39:52 -0700

    Added firebase.js file in backend folder and initialized for google auth.

---
+++ backend/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+require('dotenv').config();
+let apiKey=process.env.API_KEY;
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: "message-a7460.firebaseapp.com",
+    projectId: "message-a7460",
+    storageBucket: "message-a7460.appspot.com",
+    messagingSenderId: "952883900225",
+    appId: "1:952883900225:web:4ee17bb0c676e665d87bc0"
+  };
+
+  let firebaseApp=firebase.initializeApp(firebaseConfig);
+
+  let db=firebase.firestore();
+  let auth=firebase.auth();
+  let provider=new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
