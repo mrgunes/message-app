13eaab2293eae41037036d22ce16c8966e03e905
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-29 19:03:05 -0700

    Some rules were added for SidebarChat.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,6 +24,8 @@ function Sidebar() {
 
             <div className='sidebar__chats'>
                 <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
